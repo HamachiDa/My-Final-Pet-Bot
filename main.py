207f4ff9cc6d33fffb4ed58b5b649b159d24c238
HamachiDa <asaokitakunai4@gmail.com>
2025-11-12 01:17:16 +0900

    不要なコードを削除し、メインブロックの実行部分を整理

--- main.py
+++ main.py
@@ -394,7 +394,3 @@ def handle_message(event):
             TextSendMessage(text=response_text))
     except Exception as e:
         print(f"Failed to send reply message: {e}")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
